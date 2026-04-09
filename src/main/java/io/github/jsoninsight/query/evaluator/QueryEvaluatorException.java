28a05bcc10c833ac3e23d42be7d2eea6eff7e7b0
Filip Stępień <filip.stepien30@gmail.com>
2026-04-09 22:01:31 +0200

    feat: add query evaluator
    TODO: function call and IS operator implementation

---
+++ src/main/java/io/github/jsoninsight/query/evaluator/QueryEvaluatorException.java
@@ -0,0 +1,8 @@
+package io.github.jsoninsight.query.evaluator;
+
+public class QueryEvaluatorException extends RuntimeException {
+
+    public QueryEvaluatorException(String message) {
+        super(message);
+    }
+}
