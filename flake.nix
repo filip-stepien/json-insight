296a700ac60978872864c75aaeb003f57863074e
rafal <you@example.com>
2026-06-11 23:32:51 +0200

    chore: add pdf generator task

--- flake.nix
+++ flake.nix
@@ -16,12 +16,28 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        # LaTeX subset large enough for pandoc's default xelatex PDF output.
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-small
+            xetex
+            fontspec
+            unicode-math
+            xcolor
+            geometry
+            fancyhdr
+            booktabs
+            ;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             (pkgs.jdk21.override { enableJavaFX = true; })
             maven
+            go-task
+            pandoc
+            tex
           ];
 
         };
